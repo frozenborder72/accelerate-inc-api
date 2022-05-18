3c808d0b80d7811b1b882b9ea3a2a5b7f3fc2dc0
Marco <mar.manunta@gmail.com>
2022-05-18 18:11:00 +0200

    prepared for deployment

--- config/environment.js
+++ config/environment.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 export const port = process.env.PORT || 3000;
 
-export const dbURL = process.env.dbURL || 'mongodb://127.0.0.1:27017/project-3';
+export const DB_URI =
+  process.env.DB_URI || 'mongodb://127.0.0.1:27017/project-3';
 export const secret = process.env.SECRET || 'pineapples';
